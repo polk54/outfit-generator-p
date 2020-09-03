2e3cdd1c9d77616f93b74989ac8e48ad8dea34e9
Parul Agarwal <polk54@gmail.com>
2020-09-03 19:26:06 -0400

    Update accessory.js

--- accessory.js
+++ accessory.js
@@ -5,7 +5,9 @@ function getAccessory() {
 }
 
 var randomAccessory = new Array();
-randomAccessory[0] = "https://polk54.github.io/outfit-generator-p/images/Acc-1.PNG";
-randomAccessory[1] = "https://polk54.github.io/outfit-generator-p/images/Acc-2.PNG";
-randomAccessory[2] = "https://polk54.github.io/outfit-generator-p/images/Acc-3.PNG";
+randomAccessory[0] = "https://polk54.github.io/outfit-generator-p/images/Acc-1.jpg";
+randomAccessory[1] = "https://polk54.github.io/outfit-generator-p/images/Acc-2.jpg";
+randomAccessory[2] = "https://polk54.github.io/outfit-generator-p/images/Acc-3.jpg";
+randomAccessory[3] = "https://polk54.github.io/outfit-generator-p/images/Acc-4.jpg";
+randomAccessory[4] = "https://polk54.github.io/outfit-generator-p/images/Acc-5.jpg";
 
