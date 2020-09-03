244bffa481732180c31e9718656435d5c73f71d2
Parul Agarwal <polk54@gmail.com>
2020-09-03 11:00:41 -0400

    Update shoes.js

--- shoes.js
+++ shoes.js
@@ -4,10 +4,10 @@ function getShoes() {
   document.getElementById("shoesDisplay").innerHTML =('<img src="'+randomShoes[number]+'" />');
 }
 var randomShoes = new Array();
-randomShoes[0] = "/images/Shoes-1.PNG";
-randomShoes[1] = "/images/Shoes-2.PNG";
-randomShoes[2] = "images/Shoes-3.PNG";
-randomShoes[3] = "images/Shoes-4.PNG";
-randomShoes[4] = "images/Shoes-5.PNG";
-randomShoes[5] = "images/Shoes-6.PNG";
-randomShoes[6] = "images/Shoes-7.PNG";
+randomShoes[0] = "https://polk54.github.io/outfit-generator-p/images/Shoes-1.PNG";
+randomShoes[1] = "https://polk54.github.io/outfit-generator-p/images/Shoes-2.PNG"";
+randomShoes[2] = "https://polk54.github.io/outfit-generator-p/images/Shoes-3.PNG"";
+randomShoes[3] = "https://polk54.github.io/outfit-generator-p/images/Shoes-4.PNG"";
+randomShoes[4] = "https://polk54.github.io/outfit-generator-p/images/Shoes-5.PNG"";
+randomShoes[5] = "https://polk54.github.io/outfit-generator-p/images/Shoes-6.PNG"";
+randomShoes[6] = "https://polk54.github.io/outfit-generator-p/images/Shoes-7.PNG"";
