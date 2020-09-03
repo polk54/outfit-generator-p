a320751ea9e8031806d5606e30eedcfa5d153def
Parul Agarwal <polk54@gmail.com>
2020-09-03 10:50:23 -0400

    Update accessory.js

--- accessory.js
+++ accessory.js
@@ -5,7 +5,7 @@ function getAccessory() {
 }
 
 var randomAccessory = new Array();
-randomAccessory[0] = "https://www.dropbox.com/s/6xwtw5y3hh8a1yv/Acc-1.PNG?dl=0";
-randomAccessory[1] = "https://www.dropbox.com/s/9v7cc7a0f6zx8fc/Acc-2.PNG?dl=0";
-randomAccessory[2] = "https://www.dropbox.com/s/4sg1mibti6wp1b4/Acc-3.PNG?dl=0";
+randomAccessory[0] = "https://www.dropbox.com/s/6xwtw5y3hh8a1yv/Acc-1.PNG";
+randomAccessory[1] = "https://www.dropbox.com/s/9v7cc7a0f6zx8fc/Acc-2.PNG";
+randomAccessory[2] = "https://www.dropbox.com/s/4sg1mibti6wp1b4/Acc-3.PNG";
 
