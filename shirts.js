a5280db976f6426445a3c327feadc580bcd0b383
Parul Agarwal <polk54@gmail.com>
2020-09-03 21:00:42 -0400

    Update shirts.js

--- shirts.js
+++ shirts.js
@@ -8,3 +8,4 @@ var randomShirt = new Array();
 randomShirt[0] = "https://polk54.github.io/outfit-generator-p/images/Top-1.jpg";
 randomShirt[1] = "https://polk54.github.io/outfit-generator-p/images/Top-2.jpg";
 randomShirt[2] = "https://polk54.github.io/outfit-generator-p/images/Top-3.jpg";
+randomShirt[3] = "https://polk54.github.io/outfit-generator-p/images/Top-4.jpg";
