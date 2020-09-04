6361c0ecfca966cbecb81477eb9f49a709a34e15
Parul Agarwal <polk54@gmail.com>
2020-09-03 20:07:37 -0400

    Update shirts.js

--- shirts.js
+++ shirts.js
@@ -5,6 +5,6 @@ function getShirt() {
 }
 
 var randomShirt = new Array();
-randomShirt[0] = "https://i.s-madewell.com/is/image/madewell/MA651_PP0156_ld?wid=1400&hei=1779&fmt=jpeg&fit=crop&qlt=75,1&resMode=bisharp&op_usm=0.5,1,5,0";
-randomShirt[1] = "https://i.s-madewell.com/is/image/madewell/AN127_BK5229_ld?wid=1400&hei=1779&fmt=jpeg&fit=crop&qlt=75,1&resMode=bisharp&op_usm=0.5,1,5,0";
-randomShirt[2] = "https://i.s-madewell.com/is/image/madewell/MA571_WY2361_ld?wid=1400&hei=1779&fmt=jpeg&fit=crop&qlt=75,1&resMode=bisharp&op_usm=0.5,1,5,0";
+randomShirt[0] = "https://polk54.github.io/outfit-generator-p/images/Top-1.PNG";
+randomShirt[1] = "https://polk54.github.io/outfit-generator-p/images/Top-2.PNG";
+randomShirt[2] = "https://polk54.github.io/outfit-generator-p/images/Top-3.PNG";
